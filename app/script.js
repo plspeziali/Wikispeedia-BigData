ebd13e4848da8b5cad6298dfae449264eaf23e95
Paolo Speziali <speziali99@gmail.com>
2022-12-05 21:17:16 +0100

    Sistemata visualizzazione grafo force directed

--- app/script.js
+++ app/script.js
@@ -1,6 +1,9 @@
 const neo4j = require('neo4j-driver')
 const fs = require('fs')
 let articleNames = [];
+let nodes = [];
+let edges = [];
+var sys;
 
 document.addEventListener('DOMContentLoaded', async e => {
     const driver = neo4j.driver('bolt://54.208.199.182:7687',
@@ -28,20 +31,9 @@ document.addEventListener('DOMContentLoaded', async e => {
     // on application exit:
     await driver.close()
 
-    var sys = arbor.ParticleSystem(1000, 400,1);
+    sys = arbor.ParticleSystem(1000, 400,1);
     sys.parameters({gravity:true});
     sys.renderer = Renderer("#viewport") ;
-    var data = {
-        nodes:{
-            animals:{'color':'red','shape':'dot','label':'Animals'},
-            dog:{'color':'green','shape':'dot','label':'dog'},
-            cat:{'color':'blue','shape':'dot','label':'cat'}
-        },
-        edges:{
-            animals:{ dog:{}, cat:{} }
-        }
-    };
-    sys.graft(data);
 }, false);
 
 
@@ -49,32 +41,46 @@ $( "#pathQ" ).click(async function () {
     const driver = neo4j.driver('bolt://54.208.199.182:7687',
         neo4j.auth.basic("neo4j", "turbines-sentry-screen"))
     const session = driver.session()
+    let first = $('#firstArticle').val();
+    let second = $('#secondArticle').val()
+    if(first === second){
+        return;
+    }
     let res;
     try {
         const result = await session.run(
-            'MATCH (nodo1:Article {name: \'' + $('#firstArticle').val() + '\'}),\n' +
-            '      (nodo2:Article {name: \'' + $('#secondArticle').val() + '\'}),\n' +
+            'MATCH (nodo1:Article {name: \'' + first + '\'}),\n' +
+            '      (nodo2:Article {name: \'' + second + '\'}),\n' +
             '      p = shortestPath((nodo1)-[:RELTYPE*]-(nodo2))\n' +
             'RETURN p'
         )
 
-        var sys = arbor.ParticleSystem(1000, 400,1);
-        sys.parameters({gravity:true});
-        sys.renderer = Renderer("#viewport") ;
+        for(let n of edges){
+            sys.pruneEdge(n)
+        }
+
+        for(let n of nodes){
+            sys.pruneNode(n)
+        }
+        nodes.splice(0,nodes.length)
+
+        edges.splice(0,edges.length)
 
         for (let el of result.records) {
             let seg = el.get(0).segments;
-            console.log(seg[0])
-            for (let i = 0; i < seg.length; i+=3) {
+            console.log(seg)
+            for (let i = 0; i < seg.length; i++) {
                 name1 = seg[i].start.properties.name;
-                name2 = seg[i+2].end.properties.name;
+                name2 = seg[i].end.properties.name;
                 var n1 = sys.addNode(name1,{'color':'green','shape':'dot','label':name1});
                 var n2 = sys.addNode(name2,{'color':'blue','shape':'dot','label':name2});
-                sys.addEdge(n1, n2);
+                var e = sys.addEdge(n1, n2);
+                nodes.push(n1);
+                nodes.push(n2);
+                edges.push(e);
             }
         }
 
-        sys.graft(data);
         console.log(result)
 
 
@@ -119,189 +125,3 @@ $('.the-basics .typeahead').typeahead({
     limit:10,
     source: substringMatcher(require('./articleNames.json'))
 });
-
-[
-    {
-        "keys": [
-            "p"
-        ],
-        "length": 1,
-        "_fields": [
-            {
-                "start": {
-                    "identity": {
-                        "low": 3,
-                        "high": 0
-                    },
-                    "labels": [
-                        "Article"
-                    ],
-                    "properties": {
-                        "name": "South_America"
-                    },
-                    "elementId": "3"
-                },
-                "end": {
-                    "identity": {
-                        "low": 4,
-                        "high": 0
-                    },
-                    "labels": [
-                        "Article"
-                    ],
-                    "properties": {
-                        "name": "Gold"
-                    },
-                    "elementId": "4"
-                },
-                "segments": [
-                    {
-                        "start": {
-                            "identity": {
-                                "low": 3,
-                                "high": 0
-                            },
-                            "labels": [
-                                "Article"
-                            ],
-                            "properties": {
-                                "name": "South_America"
-                            },
-                            "elementId": "3"
-                        },
-                        "relationship": {
-                            "identity": {
-                                "low": 0,
-                                "high": 0
-                            },
-                            "start": {
-                                "low": 3,
-                                "high": 0
-                            },
-                            "end": {
-                                "low": 1,
-                                "high": 0
-                            },
-                            "type": "RELTYPE",
-                            "properties": {},
-                            "elementId": "0",
-                            "startNodeElementId": "3",
-                            "endNodeElementId": "1"
-                        },
-                        "end": {
-                            "identity": {
-                                "low": 1,
-                                "high": 0
-                            },
-                            "labels": [
-                                "Article"
-                            ],
-                            "properties": {
-                                "name": "Brazil"
-                            },
-                            "elementId": "1"
-                        }
-                    },
-                    {
-                        "start": {
-                            "identity": {
-                                "low": 1,
-                                "high": 0
-                            },
-                            "labels": [
-                                "Article"
-                            ],
-                            "properties": {
-                                "name": "Brazil"
-                            },
-                            "elementId": "1"
-                        },
-                        "relationship": {
-                            "identity": {
-                                "low": 2,
-                                "high": 0
-                            },
-                            "start": {
-                                "low": 1,
-                                "high": 0
-                            },
-                            "end": {
-                                "low": 2,
-                                "high": 0
-                            },
-                            "type": "RELTYPE",
-                            "properties": {},
-                            "elementId": "2",
-                            "startNodeElementId": "1",
-                            "endNodeElementId": "2"
-                        },
-                        "end": {
-                            "identity": {
-                                "low": 2,
-                                "high": 0
-                            },
-                            "labels": [
-                                "Article"
-                            ],
-                            "properties": {
-                                "name": "Africa"
-                            },
-                            "elementId": "2"
-                        }
-                    },
-                    {
-                        "start": {
-                            "identity": {
-                                "low": 2,
-                                "high": 0
-                            },
-                            "labels": [
-                                "Article"
-                            ],
-                            "properties": {
-                                "name": "Africa"
-                            },
-                            "elementId": "2"
-                        },
-                        "relationship": {
-                            "identity": {
-                                "low": 4,
-                                "high": 0
-                            },
-                            "start": {
-                                "low": 2,
-                                "high": 0
-                            },
-                            "end": {
-                                "low": 4,
-                                "high": 0
-                            },
-                            "type": "RELTYPE",
-                            "properties": {},
-                            "elementId": "4",
-                            "startNodeElementId": "2",
-                            "endNodeElementId": "4"
-                        },
-                        "end": {
-                            "identity": {
-                                "low": 4,
-                                "high": 0
-                            },
-                            "labels": [
-                                "Article"
-                            ],
-                            "properties": {
-                                "name": "Gold"
-                            },
-                            "elementId": "4"
-                        }
-                    }
-                ],
-                "length": 3
-            }
-        ],
-        "_fieldLookup": {
-            "p": 0
-        }
-    }
-]
